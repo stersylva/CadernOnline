b85f231a2339d4980c6ff94863b4d0dbacab6aa0
stersylva <stersylva@gmail.com>
2017-05-19 00:10:02 -0300

    Mensagem salvar

--- app/src/main/java/com/example/ster_lenovo/cadernonline/Instituicao_addActivity.java
+++ app/src/main/java/com/example/ster_lenovo/cadernonline/Instituicao_addActivity.java
@@ -2,6 +2,9 @@ package com.example.ster_lenovo.cadernonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Instituicao_addActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Instituicao_addActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instituicao_add);
+
+        Button botaoSalvar = (Button) findViewById(R.id.btn_inst_salvar);
+        botaoSalvar.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Instituicao_addActivity.this, "Instituição Salva Com Sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
